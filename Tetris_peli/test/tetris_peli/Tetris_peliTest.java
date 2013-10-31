10e65a3d79ba5de5402763fc041ba424d8756b09
Johanna C Aijala <joaijala@dhp-29.cs.helsinki.fi>
2013-10-31 13:13:33 +0200

    testi paketti lisätty

---
+++ Tetris_peli/test/tetris_peli/Tetris_peliTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris_peli;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joaijala
+ */
+public class Tetris_peliTest {
+    
+    public Tetris_peliTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Tetris_peli.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Tetris_peli.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
